892074cdfe6e7043c2d0ed923d1f17f0db1dae44
Gary Gurlaskie <garygurlaskie@gmail.com>
2020-04-21 22:04:13 -0400

    Delete syntax_highlight.pas

--- other/syntax_highlight.pas
+++
@@ -1,7 +0,0 @@
-the quick 'brown' fox 1 2 3 '1234' '12''34' -1 -2 -3 -3.0 procedure program then to // asdf asdf asdf
-the (*comment*) brown '(*not comment*)' asdf { my comment 'weeee' } 'asdf {asdf}' {
-    a multi
-    line comment
-} not commented yay (* 
-these work as
-well for multiple lines *)
